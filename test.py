c7816006116a1aee81b677642c29a2f51147c3f6
Ubuntu <ubuntu@se1700-gpu-spot-big-1.ftp.sh>
2015-05-26 09:27:46 +0000

    modify nms

--- test.py
+++ test.py
@@ -42,34 +42,22 @@ def generateBoundingBox(featureMap, scale):
     cellSize = 227
     #227 x 227 cell, stride=32
     for (x,y), prob in np.ndenumerate(featureMap):
-       if(prob >= 0.9):
+       if(prob >= 0.85):
             boundingBox.append([float(stride * y)/ scale, float(x * stride)/scale, float(stride * y + cellSize - 1)/scale, float(stride * x + cellSize - 1)/scale, prob])
     #sort by prob, from max to min.
     #boxes = np.array(boundingBox)
     return boundingBox
-#def nms_average(boxes, overlapThresh=0.2):
-
-
-
-
-def nms_max(boxes, overlapThresh=0.3):
+def nms_average(boxes, overlapThresh=0.2):
+    result_boxes = []
     if len(boxes) == 0:
         return []
-
-    # if the bounding boxes integers, convert them to floats --
-    # this is important since we'll be doing a bunch of divisions
-    #if boxes.dtype.kind == "i":
-    #    boxes = boxes.astype("float")
-
     # initialize the list of picked indexes
     pick = []
-
     # grab the coordinates of the bounding boxes
     x1 = boxes[:,0]
     y1 = boxes[:,1]
     x2 = boxes[:,2]
     y2 = boxes[:,3]
-
     # compute the area of the bounding boxes and sort the bounding
     # boxes by the bottom-right y-coordinate of the bounding box
     area = (x2 - x1 + 1) * (y2 - y1 + 1)
@@ -101,7 +89,89 @@ def nms_max(boxes, overlapThresh=0.3):
         area_array.fill(area_i)
         # compute the ratio of overlap
         #overlap = (w * h) / (area[idxs[:last]]  - w * h + area_array)
-        overlap = (w * h) / (area[idxs[:last]])
+        
+	overlap = (w * h) / (area[idxs[:last]])
+	delete_idxs = np.concatenate(([last],np.where(overlap > overlapThresh)[0]))
+        xmin = 10000
+	ymin = 10000
+	xmax = 0
+	ymax = 0
+	ave_prob  = 0
+	width = x2[i] - x1[i] + 1
+	height = y2[i] - y1[i] + 1
+	for idx in delete_idxs:
+		ave_prob += boxes[idxs[idx]][4]
+		if(boxes[idxs[idx]][0] < xmin):
+			xmin = boxes[idxs[idx]][0]
+		if(boxes[idxs[idx]][1] < ymin):
+			ymin = boxes[idxs[idx]][1]
+		if(boxes[idxs[idx]][2] > xmax):
+			xmax = boxes[idxs[idx]][2]
+		if(boxes[idxs[idx]][3] > ymax):
+			ymax = boxes[idxs[idx]][3]
+	if(x1[i] - xmin >  0.1 * width):
+		xmin = x1[i] - 0.1 * width
+	if(y1[i] - ymin > 0.1 * height):
+		ymin = y1[i] - 0.1 * height
+	if(xmax - x2[i]> 0.1 * width):
+		xmax = x2[i]  + 0.1 * width
+	if( ymax - y2[i] > 0.1 * height):
+		ymax = y2[i] + 0.1 * height
+	result_boxes.append([xmin, ymin, xmax, ymax, ave_prob / len(delete_idxs)])
+	# delete all indexes from the index list that have
+        idxs = np.delete(idxs, delete_idxs)
+
+    # return only the bounding boxes that were picked using the
+    # integer data type
+    #result = np.delete(boxes[pick],np.where(boxes[pick][:, 4] < 0.9)[0],  axis=0)
+    #print boxes[pick]
+    return result_boxes
+
+
+
+
+def nms_max(boxes, overlapThresh=0.3):
+    if len(boxes) == 0:
+        return []
+    # initialize the list of picked indexes
+    pick = []
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:,0]
+    y1 = boxes[:,1]
+    x2 = boxes[:,2]
+    y2 = boxes[:,3]
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(boxes[:,4])
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+         # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+        #area of i.
+        area_i = np.maximum(0, x2[i] - x1[i] + 1) * np.maximum(0, y2[i] - y1[i] + 1)
+        area_array = np.zeros(len(idxs) - 1)
+        area_array.fill(area_i)
+        # compute the ratio of overlap
+        overlap = (w * h) / (area[idxs[:last]]  - w * h + area_array)
+        #overlap = (w * h) / (area[idxs[:last]])
         # delete all indexes from the index list that have
         idxs = np.delete(idxs, np.concatenate(([last],np.where(overlap > overlapThresh)[0])))
 
@@ -143,17 +213,16 @@ def face_detection(imgList):
     else:
         min = img.size[0]
 	max = img.size[1]
-    if(max < 2000):
-   	scales.append(1)
-    if(max * 3 < 2000): 
-    	scales.append(3)
-    elif(max * 2 < 2000):
-    	scales.append(2)
+    delim = 2500/max
+    if(delim == 1):
+	scales.append(1)
+    elif(delim > 1):
+        scales.append(delim)
     
     #scales.append(5)
     min = min * factor
     factor_count = 1
-    while(min >= 500):
+    while(min >= 227):
         scales.append(pow(factor,  factor_count))
         min = min * factor
         factor_count += 1
@@ -213,7 +282,8 @@ def face_detection(imgList):
 
     #nms
     boxes_nms = np.array(total_boxes)
-    true_boxes = nms_max(boxes_nms, overlapThresh=0.3)
+    true_boxes1 = nms_max(boxes_nms, overlapThresh=0.3)
+    true_boxes = nms_average(np.array(true_boxes1), overlapThresh=0.07)
     #display the nmx bounding box in  image.
     draw = ImageDraw.Draw(img)
     print "width:", img.size[0], "height:",  img.size[1]
@@ -228,4 +298,4 @@ def face_detection(imgList):
 
 if __name__ == "__main__":
     #convert_full_conv()
-    face_detection("afw.txt")
+    face_detection("/mnt_data/data/yilin_guo/pipa_new/val.txt")
